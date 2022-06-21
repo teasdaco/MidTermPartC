a948c32d54ff365488afb48ac95e61b3d5c37a07
teasdaco <105743439+teasdaco@users.noreply.github.com>
2022-06-21 12:46:53 -0400

    added a colin class

---
+++ src/colin/java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package colin;
+
+/**
+ *
+ * SYST17796 Fundamentals of Software Design and Development
+ * @author Colin Teasdale   
+ */
+public class java {
+    //These are some comments
+    //These are some more comments
+}
